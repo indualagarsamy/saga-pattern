8459e3b30d62f17d3fc86f9d0a5d2dad997ba263
indualagarsamy <indu.alagarsamy@gmail.com>
2020-03-02 23:45:02 +0000

    Added tests for timeouts

--- src/CompleteSolutionWithTimeouts/AmericanAirlines.PreferredCustomerPolicy/Sagas/PreferredCustomerGoldPolicy.cs
+++ src/CompleteSolutionWithTimeouts/AmericanAirlines.PreferredCustomerPolicy/Sagas/PreferredCustomerGoldPolicy.cs
@@ -6,7 +6,7 @@ using Events;
 namespace PreferredCustomerPolicy.Sagas
 {
 
-    class PreferredCustomerGoldPolicy : Saga<PreferredCustomerGoldPolicyData>,
+    public class PreferredCustomerGoldPolicy : Saga<PreferredCustomerGoldPolicyData>,
         IAmStartedByMessages<FlightPlanWasAdded>,
         IAmStartedByMessages<CustomerWasBilled>,
         IHandleTimeouts<CalendarYearHasStarted>
@@ -57,6 +57,7 @@ namespace PreferredCustomerPolicy.Sagas
         {
             await context.Publish(new GoldStatusActivated() { CustomerId = Data.CustomerId }).ConfigureAwait(false);
             Console.WriteLine("Publishing GoldStatusactivated event");
+            MarkAsComplete();
         }
     }
 
